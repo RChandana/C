108c51e1f0e73e3058cbc8150699d7121a916816
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-06-01 23:16:10 +0530

    Update Basics.c

--- Basics.c
+++ Basics.c
@@ -20,3 +20,15 @@ int main()
 }
 
 
+/* storage space taken by a float type and its range values */
+#include <stdio.h>
+#include <float.h>
+int main()
+{
+ printf("Storage size for float : %d \n", sizeof(float));
+ printf("Minimum float positive value: %E\n", FLT_MIN );
+ printf("Maximum float positive value: %E\n", FLT_MAX );
+ printf("Precision value: %d\n", FLT_DIG );
+
+ return 0;
+}
